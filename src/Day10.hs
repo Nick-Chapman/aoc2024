a652b1c80066f39b15739ca10ab78bd3a65b742c
Nick Chapman <nic@amadido.co.uk>
2024-12-11 07:41:47 +0000

    day10(part2)

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,11 +1,9 @@
 module Day10 (main) where
 
 import Data.Map (Map)
-import Data.Set (Set)
-import Misc (check)
+import Misc (check,nub)
 import Par4 (parse,Par,separated,nl,many,digit)
 import qualified Data.Map as Map
-import qualified Data.Set as Set
 
 main :: IO ()
 main = do
@@ -13,6 +11,8 @@ main = do
   inp <- parse gram <$> readFile "input/day10.input"
   print ("day10, part1 (sample)", check 36 $ part1 sam)
   print ("day10, part1", check 786 $ part1 inp)
+  print ("day10, part1 (sample)", check 81 $ part2 sam)
+  print ("day10, part1", check 1722 $ part2 inp)
 
 type Top = Map Pos Int
 type Pos = (Int,Int)
@@ -21,21 +21,24 @@ gram :: Par Top
 gram = makeTop <$> separated nl (many digit)
 
 makeTop :: [[Int]] -> Top
-makeTop dss = do
-  Map.fromList [ ((x,y),d) | (y,ds) <- zip [0..] dss, (x,d) <- zip [0..] ds ]
+makeTop dss = Map.fromList [ ((x,y),d) | (y,ds) <- zip [0..] dss, (x,d) <- zip [0..] ds ]
 
 part1 :: Top -> Int
-part1 top =
-  sum [ Set.size dest | h <- trailheads top , let dest = hike top 0 (Set.singleton h) ]
+part1 top = sum [ length (nub (hike top 0 h)) | h <- trailheads top ]
+
+part2 :: Top -> Int
+part2 top = sum [ length (hike top 0 h) | h <- trailheads top ]
 
 trailheads :: Top -> [Pos]
 trailheads top = [ p | (p,0) <- Map.toList top ]
 
-hike :: Top -> Int -> Set Pos -> Set Pos
-hike top n ps = do
-  if n == 9 then ps else do
-    hike top (n+1) $
-      Set.fromList [ q | p <- Set.toList ps, q <- adj p, Map.lookup q top == Just (n+1) ]
+hike :: Top -> Int -> Pos -> [Pos]
+hike top n p = do
+  if n == 9 then [p] else do
+    [ d
+      | q <- adj p, Map.lookup q top == Just (n+1)
+      , d <- hike top (n+1) q
+      ]
 
 adj :: Pos -> [Pos]
 adj (x,y) = [ (x,y-1), (x,y+1), (x-1,y), (x+1,y) ]
