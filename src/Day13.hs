c50e5127c264709609f5cdb4dfaad14916646351
Nick Chapman <nic@amadido.co.uk>
2024-12-16 10:00:29 +0000

    day13(part2)

--- src/Day13.hs
+++ src/Day13.hs
@@ -7,43 +7,33 @@ main :: IO ()
 main = do
   sam <- parse gram <$> readFile "input/day13.sample"
   inp <- parse gram <$> readFile "input/day13.input"
-  print ("day13, part1 (sample)", check 480 $ part1 sam)
-  print ("day13, part1", check 32026 $ part1 inp)
+  print ("day13, part1 (sample)", check 480 $ go sam)
+  print ("day13, part1", check 32026 $ go inp)
+  print ("day13, part2", check 89013607072065 $ go (map correct inp))
 
-type Input = [Puz]
+correct :: Puz -> Puz
+correct (a,b,c,d,e,f) = (a,b,c,d,e+big,f+big) where big = 10000000000000
+
 type Puz = (Int,Int,Int,Int,Int,Int)
 
-gram :: Par Input
+gram :: Par [Puz]
 gram = separated (do nl;nl) puz
   where
     puz = do
-      key "Button A: X+"
-      a <- int
-      key ", Y+"
-      b <- int
-      nl
-      key "Button B: X+"
-      c <- int
-      key ", Y+"
-      d <- int
-      nl
-      key "Prize: X="
-      e <- int
-      key ", Y="
-      f <- int
+      key "Button A: X+"; a <- int; key ", Y+"; c <- int; nl
+      key "Button B: X+"; b <- int; key ", Y+"; d <- int; nl
+      key "Prize: X="; e <- int; key ", Y="; f <- int
       pure (a,b,c,d,e,f)
 
-part1 :: Input -> Int
-part1 = sum . map solve
+go :: [Puz] -> Int
+go = sum . map solve
 
 solve :: Puz -> Int
 solve (a,b,c,d,e,f) = do
-  let
-    xs =
-      [ 3*i + j
-      | i <- [0..100]
-      , j <- [0..100]
-      , i*a + j*c == e
-      , i*b + j*d == f
-      ]
-  case xs of [] -> 0; _ -> minimum xs
+  let det = a*d - b*c
+  if (det == 0) then error "det0" else do
+    let idet = d*e + (-b)*f
+    let jdet = (-c)*e + a*f
+    if (idet `mod` det == 0) && (jdet `mod` det == 0)
+    then 3* (idet `div` det) + jdet `div` det
+    else 0
