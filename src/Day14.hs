a9341be9c0007d23d6f8ef9582fbf16845e573d6
Nick Chapman <nic@amadido.co.uk>
2024-12-31 17:33:53 +0000

    day14(part2): search. Explicit -O2.

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,9 +1,11 @@
 module Day14 (main) where
 
 import Control.Monad (forM_)
-import Data.Map as Map
+import Data.Ord (comparing)
 import Misc (check,hist)
 import Par4 (parse,Par,separated,nl,lit,key,int,alts)
+import qualified Data.List as List (minimumBy)
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
@@ -13,29 +15,11 @@ main = do
   let boundInp = (101,103)
   print ("day14, part1 (sample)", check 12 $ part1 boundSam sam)
   print ("day14, part1", check 221616000 $ part1 boundInp inp)
-  let _ = searchPart2 10000 boundInp inp
-  part2 7572 boundInp inp
-
-searchPart2 :: Int -> Pos -> Input -> IO ()
-searchPart2 max bound rs = do
-  forM_ [0..max] $ \nSteps -> do
-    print nSteps
-    putStrLn (pic bound [ locate bound nSteps r | r <- rs ])
-
-part2 :: Int -> Pos -> Input -> IO ()
-part2 nSteps bound rs =
-  putStrLn (pic bound [ locate bound nSteps r | r <- rs ])
-
-pic :: Pos -> [Pos] -> String
-pic (xB,yB) ps = do
-  let h = hist ps
-  let
-    cell x y =
-      case Map.lookup (x,y) h of
-        Nothing -> "."
-        Just n -> show n
-  unlines [ concat [ cell x y | x <- [0..xB-1] ] | y <- [0..yB-1] ]
-
+  let _ = searchPart2 10000 boundInp inp -- print pics
+  let part2_inp = part2 boundInp inp
+  print ("day14, part2", check 7572 $ part2_inp)
+  let _ = pp part2_inp boundInp inp -- picture result
+  pure ()
 
 type Pos = (Int,Int)
 type Robot = (Pos,Pos)
@@ -49,8 +33,14 @@ gram = separated nl rob
     sint = alts [int , negate <$> do lit '-'; int]
 
 part1 :: Pos -> Input -> Int
-part1 bound rs = do
-  let nSteps = 100
+part1 = safetyFactor 100
+
+part2 :: Pos -> Input -> Int
+part2 bound rs = do
+  fst $ List.minimumBy (comparing snd) $ [ (n, safetyFactor n bound rs) | n <- [ 0..10000 ] ]
+
+safetyFactor :: Int -> Pos -> Input -> Int
+safetyFactor nSteps bound rs = do
   let qs = [ locate bound nSteps r | r <- rs ]
   let (a,b,c,d) = quaterize bound qs
   a*b*c*d
@@ -69,3 +59,22 @@ locate :: Pos -> Int -> Robot -> Pos
 locate (xB,yB) n ((xI,yI),(xV,yV)) = (calc xB xI xV, calc yB yI yV)
   where
     calc b i v = (i + n * v) `mod` b
+
+searchPart2 :: Int -> Pos -> Input -> IO ()
+searchPart2 max bound rs = do
+  forM_ [0..max] $ \nSteps -> do
+    print nSteps
+    pp nSteps bound rs
+
+pp :: Int -> Pos -> Input -> IO ()
+pp nSteps bound rs = putStrLn (pic bound [ locate bound nSteps r | r <- rs ])
+
+pic :: Pos -> [Pos] -> String
+pic (xB,yB) ps = do
+  let h = hist ps
+  let
+    cell x y =
+      case Map.lookup (x,y) h of
+        Nothing -> "."
+        Just n -> show n
+  unlines [ concat [ cell x y | x <- [0..xB-1] ] | y <- [0..yB-1] ]
