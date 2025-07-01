4539af388c1d3ee8da5f0f5fd0e01ed00c31815e
Nick Chapman <nic@amadido.co.uk>
2025-07-01 16:58:05 +0100

    split out Timing code to separate module

--- src/Advent2024.hs
+++ src/Advent2024.hs
@@ -1,12 +1,7 @@
-module Advent2024 where
+module Advent2024 (main) where
 
-import Control.Monad (forM_)
-import Data.List (sortBy)
-import Data.Ord (comparing)
-import GHC.Int (Int64)
-import System.Clock (TimeSpec(..),getTime,Clock(Monotonic))
 import System.Environment (getArgs)
-import Text.Printf (printf)
+import Timing (timed,printTimings)
 
 import qualified Day1
 import qualified Day2
@@ -70,30 +65,3 @@ main = do
   let picked = [ x | x@(i,_) <- mains, i `elem` selected ]
   _info <- sequence [ timed day io | (day,io) <- picked ]
   if length picked > 1 then printTimings _info else pure ()
-
-data Timing = Timing { day :: Int, time :: Nanos }
-
-timed :: Int -> IO () -> IO Timing
-timed day io = do
-  before <- getTime Monotonic
-  io
-  after <- getTime Monotonic
-  let TimeSpec{sec,nsec} = after - before
-  let time = Nanos (gig * sec + nsec)
-  pure $ Timing {day,time}
-
-printTimings :: [Timing] -> IO ()
-printTimings xs = do
-  putStrLn "\ntimings:"
-  forM_ (sortBy (comparing time) xs) $ \Timing{day,time} -> do
-    putStrLn (printf "- day %2d : " day ++ show time)
-  putStrLn $ "\ntotal = " ++ show (sum (map time xs))
-
-newtype Nanos = Nanos Int64 deriving (Eq,Ord,Num)
-
-instance Show Nanos where
-  show (Nanos i) = printf "%.03fs" dub
-    where dub :: Double = fromIntegral i / fromIntegral gig
-
-gig :: Int64
-gig = 1_000_000_000
