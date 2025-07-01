46e9063826d725324f99cb8455629efb31c6ee2e
Nick Chapman <nic@amadido.co.uk>
2025-07-01 17:25:27 +0100

    setup jenga build rules; comment out use of System.Clock in problem clock package

--- src/Advent2024.hs
+++ src/Advent2024.hs
@@ -1,7 +1,7 @@
 module Advent2024 (main) where
 
 import System.Environment (getArgs)
-import Timing (timed,printTimings)
+--import Timing (timed,printTimings)
 
 import qualified Day1
 import qualified Day2
@@ -63,5 +63,6 @@ main = do
   args <- getArgs
   let selected = if args == [] then map fst mains else map read args
   let picked = [ x | x@(i,_) <- mains, i `elem` selected ]
-  _info <- sequence [ timed day io | (day,io) <- picked ]
-  if length picked > 1 then printTimings _info else pure ()
+  --_info <- sequence [ timed day io | (day,io) <- picked ]
+  --if length picked > 1 then printTimings _info else pure ()
+  sequence_ [ io | (_,io) <- picked ]
